82af2fb91afbac9909ccb596f21364bac9041717
rishikant42 <rksbtp@gmail.com>
2016-09-18 14:22:56 +0530

    another way of sorting in decreasing order

---
+++ insertion_sort/isort4.clj
@@ -0,0 +1,22 @@
+(defn insert
+  [l k]
+  (concat
+    (filter #(<= k %) l)
+    [k]
+    (filter #(> k %) l)))
+
+(defn isort
+  [list]
+  (loop [sorted-list []
+        unsorted-list list]
+   (if (empty? unsorted-list) 
+     sorted-list
+     (recur (insert sorted-list (first unsorted-list))
+            (rest unsorted-list)))))
+
+(println (isort '(3 5 2 4 1 7 3 2 6 4 5)))
+
+;; OUTPUT
+;; 
+;; $ clojure isort4.clj 
+;; (7 6 5 5 4 4 3 3 2 2 1)
