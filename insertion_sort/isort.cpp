2f12bd98cb28533c0d3632f7fa3a964353b021ee
rishikant42 <rksbtp@gmail.com>
2016-09-30 21:01:19 +0530

    added insertion sort program in c++

---
+++ insertion_sort/isort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int a[6] = {8,2,7,9,3,6};
+  int i,j,key;
+  for(j=1;j<6;j++)
+  {
+    key = a[j];
+    i = j-1;
+    while(i>=0 && a[i]>key)
+    {
+      a[i+1] = a[i];
+      i = i-1;
+    }
+    a[i+1] = key;
+  }
+
+  for(i=0;i<6;i++)
+    cout<<a[i]<<" ";
+
+  cout<<"\n";
+  return 0;
+}
